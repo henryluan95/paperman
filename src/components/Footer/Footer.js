7743d12503bd1ef81085ddb80eec5b553a8ac0fa
Henry Luan <henryluan95@gmail.com>
2022-06-25 16:45:05 -0400

    Create Footer folder

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p>
+        copyright &copy; Henry Luan
+        <span id="date"></span>. all rights reserved
+      </p>
+      <a href="http://www.freepik.com">Banner Image: Designed by Freepik</a>
+    </div>
+  );
+};
+
+export default Footer;
