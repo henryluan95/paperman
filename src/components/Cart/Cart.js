0a9e1892941546fe5507b644bd4647c222f8a904
Henry Luan <henryluan95@gmail.com>
2022-06-26 13:06:05 -0400

    Styled cart product

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,14 +1,13 @@
 import "./Cart.scss";
 import productsData from "../../data/products.json";
-
-import React from "react";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
   const image1 = productsData.items[0].fields.image.fields.file.url;
 
   return (
     <div className="cart">
-      <h4>Your Cart</h4>
+      <h4 className="cart__title">Your Cart</h4>
       <div className="cart__products">
         <div className="cart__product">
           <img
@@ -16,22 +15,27 @@ const Cart = () => {
             src={image1}
             alt="added to cart case"
           />
-          <div className="cart__top">
-            <p className="cart__product-title">some title</p>
-            <button className="cart__product-delete">Delete</button>
-          </div>
-          <div className="cart__bottom">
+          <div className="cart__info">
+            <div className="cart__header">
+              <p className="cart__product-title">Pink Peach</p>
+              <button className="cart__product-delete">Delete</button>
+            </div>
+
             <div className="cart__product-quantity">
               <button className="cart__product-decrease">-</button>
               <span className="cart__product-counter">1</span>
               <button className="cart__product-increase">+</button>
             </div>
+
             <p className="cart__total">
               Total: <span className="cart__total-counter">$9.99</span>
             </p>
           </div>
         </div>
       </div>
+      <Link to="#" className="cart__checkout">
+        Check Out
+      </Link>
     </div>
   );
 };
