4514d354c150624350f6b366261ef981e90e0ba9
Henry Luan <henryluan95@gmail.com>
2022-07-05 11:03:48 -0400

    Created checkout page

--- src/pages/CheckoutPage/CheckoutPage.js
+++ src/pages/CheckoutPage/CheckoutPage.js
@@ -1,19 +1,38 @@
 import "./CheckoutPage.scss";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ProductsContext } from "../../App";
 import { v4 as uuid } from "uuid";
+import visa from "../../assets/images/visa.svg";
+import mastercard from "../../assets/images/mastercard.svg";
+import express from "../../assets/images/express.svg";
+import discover from "../../assets/images/discover.svg";
+import jcb from "../../assets/images/jcb.png";
 
 const CheckoutPage = () => {
   const cart = useContext(ProductsContext);
+  const [isSameAsShipping, setIsSameAsShipping] = useState(true);
 
+  //Create a function to track subtotal
   const findSubtotal = () => {
     let subtotal = 0;
     cart.forEach((product) => (subtotal += product.price * product.quantity));
     return subtotal;
   };
 
+  //Create a function to track total
   const findTotal = (subtotal, discount = 0, shipping = 0) => {
     return subtotal - discount + shipping;
+  };
+
+  //Create a function to keep track of billing refill checkbox
+  const handleCheck = (e) => {
+    setIsSameAsShipping(!isSameAsShipping);
+    console.log(e.target.checked);
+  };
+
+  //Create a function to handle submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
   };
 
   const checkingOutProducts = cart.map((product) => {
@@ -60,7 +79,7 @@ const CheckoutPage = () => {
               <span>${findTotal(findSubtotal())}</span>
             </div>
           </div>
-          <div className="checkout__forms">
+          <form className="checkout__forms" onSubmit={handleSubmit}>
             <div className="checkout__shipping shipping">
               <h2 className="shipping__title">Shipping</h2>
               <label className="shipping__label">
@@ -112,58 +131,134 @@ const CheckoutPage = () => {
                 />
               </label>
             </div>
-            <div className="checkout__shipping shipping">
-              <h2 className="shipping__title">Billing</h2>
-              <label className="shipping__label">
-                Full Name
+            <label className="checkout__billing-refill">
+              <input
+                type="checkbox"
+                className="checkout__checkbox"
+                value={isSameAsShipping}
+                onChange={handleCheck}
+                defaultChecked={isSameAsShipping}
+              />
+              Same As Billing Address
+            </label>
+            {!isSameAsShipping && (
+              <div className="checkout__shipping billing">
+                <h2 className="billing__title">Billing</h2>
+                <label className="billing__label">
+                  Full Name
+                  <input
+                    type="text"
+                    className="billing__input"
+                    name="billingFullName"
+                  />
+                </label>
+                <label className="billing__label">
+                  Address
+                  <input
+                    type="text"
+                    className="billing__input"
+                    name="billingAddress"
+                  />
+                </label>
+                <label className="billing__label">
+                  City
+                  <input
+                    type="text"
+                    className="billing__input"
+                    name="billingCity"
+                  />
+                </label>
+                <label className="billing__label">
+                  Country
+                  <input
+                    type="text"
+                    className="billing__input"
+                    name="billingCountry"
+                  />
+                </label>
+                <label className="billing__label">
+                  Postal Code
+                  <input
+                    type="text"
+                    className="billing__input"
+                    name="billingPostalCode"
+                  />
+                </label>
+                <label className="billing__label">
+                  Phone Number
+                  <input
+                    type="text"
+                    className="billing__input"
+                    name="billingPhoneNumber"
+                  />
+                </label>
+              </div>
+            )}
+            <div className="checkout__payment payment">
+              <h2 className="payment__title">Payment</h2>
+              <div className="payment__icons">
+                <img src={visa} alt="visa" className="payment__icon" />
+                <img
+                  src={mastercard}
+                  alt="mastercard"
+                  className="payment__icon"
+                />
+                <img src={express} alt="express" className="payment__icon" />
+                <img src={discover} alt="discover" className="payment__icon" />
+                <img src={jcb} alt="jcb" className="payment__icon" />
+              </div>
+              <label className="payment__label">
+                Name on Card
                 <input
                   type="text"
-                  className="shipping__input"
-                  name="billingFullName"
+                  className="payment__input"
+                  name="paymentNameOnCard"
+                  placeholder="Michael Kalinichenko"
                 />
               </label>
-              <label className="shipping__label">
-                Address
+              <label className="payment__label">
+                Credit card number
                 <input
                   type="text"
-                  className="shipping__input"
-                  name="billingAddress"
+                  className="payment__input"
+                  name="paymentCreditCardNumber"
+                  placeholder="1111-2222-3333-4444"
                 />
               </label>
-              <label className="shipping__label">
-                City
+              <label className="payment__label">
+                Exp Month
                 <input
                   type="text"
-                  className="shipping__input"
-                  name="billingCity"
+                  className="payment__input"
+                  name="paymentExpMonth"
+                  placeholder="September"
                 />
               </label>
-              <label className="shipping__label">
-                Country
-                <input
-                  type="text"
-                  className="shipping__input"
-                  name="billingCountry"
-                />
-              </label>
-              <label className="shipping__label">
-                Postal Code
-                <input
-                  type="text"
-                  className="shipping__input"
-                  name="billingPostalCode"
-                />
-              </label>
-              <label className="shipping__label">
-                Phone Number
-                <input
-                  type="text"
-                  className="shipping__input"
-                  name="billingPhoneNumber"
-                />
-              </label>
+              <div className="payment__year-cvv">
+                <label className="payment__label">
+                  Exp Year
+                  <input
+                    type="text"
+                    className="payment__input"
+                    name="paymentExpYear"
+                    placeholder="2018"
+                  />
+                </label>
+                <label className="payment__label">
+                  CVV
+                  <input
+                    type="text"
+                    className="payment__input"
+                    name="paymentCVV"
+                    placeholder="352"
+                  />
+                </label>
+              </div>
             </div>
-          </div>
+            <button className="button checkout__place-order">
+              Place Order
+            </button>
+          </form>
         </div>
       </div>
     </div>
