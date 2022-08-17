d5e5ec140c834843ed78992d054674cdbc31c4e9
Henry Luan <henryluan@MBPl20C02GK32GQ05D.localdomain>
2022-08-17 09:35:19 -0400

    Changed the positioning of the focusbar to create a smooth scrool effect

--- src/components/FocusBar/FocusBar.js
+++ src/components/FocusBar/FocusBar.js
@@ -6,11 +6,10 @@ const FocusBar = () => {
   const bottomBar = useRef(null);
 
   const onMouseMove = (e) => {
-    topBar.current.style.left = e.pageX + "px";
-    topBar.current.style.top = e.pageY + 650 + "px";
-    bottomBar.current.style.left = e.pageX + "px";
-    bottomBar.current.style.top = e.pageY - 650 + "px";
-    console.log(e.pageY);
+    topBar.current.style.left = e.clientX + "px";
+    topBar.current.style.top = e.clientY - 650 + "px";
+    bottomBar.current.style.left = e.clientX + "px";
+    bottomBar.current.style.top = e.clientY + 650 + "px";
   };
 
   useEffect(() => {
@@ -24,7 +23,11 @@ const FocusBar = () => {
   return (
     <>
       <div ref={topBar} className="focus-bar__top"></div>
-      <div ref={bottomBar} className="focus-bar__bottom"></div>
+      <div
+        ref={bottomBar}
+        className="focus-bar__bottom"
+        // style={{ backgroundColor: "Orange" }}
+      ></div>
     </>
   );
 };
